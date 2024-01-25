3bc61851cd2321eabce49cd20c932ebbc13c6f35
Doplax <doplax@gmail.com>
2024-01-25 18:49:36 +0100

    feat(storage): CRUD implementation for the endpoint /storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,63 +1,83 @@
-const { storageModel } = require('../models')
-const PUBLIC_URL = process.env.PUBLIC_URL
+const fs = require("fs");
+const { matchedData } = require("express-validator");
+const { storageModel } = require("../models");
+const { handleHttpError } = require("../utils/handleError");
 
-/** 
- * 
- * @param {*} req 
- * @param {*} res 
+const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
+/**
+ * Obtener lista de la base de datos!
+ * @param {*} req
+ * @param {*} res
  */
 const getItems = async (req, res) => {
+  try {
     const data = await storageModel.find({});
-    res.send({data})
-}
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "ERROR_LIST_ITEMS");
+  }
+};
 
 /**
- * 
- * @param {*} req 
- * @param {*} res 
+ * Obtener un detalle
+ * @param {*} req
+ * @param {*} res
  */
-const getItem = (req, res) => {
-
-}
-
+const getItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req);
+    const data = await storageModel.findById(id);
+    res.send({ data });
+  } catch (e) {
+    console.log(e)
+    handleHttpError(res, "ERROR_DETAIL_ITEMS");
+  }
+};
 
 /**
- * 
- * @param {*} req 
- * @param {*} res 
+ * Insertar un registro
+ * @param {*} req
+ * @param {*} res
  */
 const createItem = async (req, res) => {
-    const { body, file } = req;
-    console.log(file);
+  try {
+    const { file } = req;
     const fileData = {
-        dilename: file.filename,
-        url:`${PUBLIC_URL}/${file.filename}`
-    }
+      filename: file.filename,
+      url: `${PUBLIC_URL}/${file.filename}`,
+    };
     const data = await storageModel.create(fileData);
-    res.send({data})
-}
+    res.status(201);
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "ERROR_DETAIL_ITEMS");
+  }
+};
 
 /**
- * 
- * @param {*} req 
- * @param {*} res 
+ * Eliminar un registro
+ * @param {*} req
+ * @param {*} res
  */
-const updateItem = (req, res) => {}
+const deleteItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req);
+    const dataFile = await storageModel.findById(id);
+    const deleteResponse = await storageModel.delete({ _id: id });
+    const { filename } = dataFile;
+    const filePath = `${MEDIA_PATH}/${filename}`; //TODO c:/miproyecto/file-1232.png
 
+    // fs.unlinkSync(filePath);
+    const data = {
+      filePath,
+      deleted: deleteResponse.matchedCount,
+    };
 
-/**
- * 
- * @param {*} req 
- * @param {*} res 
- */
-const deleteItem = (req, res) => {}
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "ERROR_DETAIL_ITEMS");
+  }
+};
 
-
-
-module.exports = {
-    getItem,
-    getItems,
-    createItem,
-    updateItem,
-    deleteItem
-}
+module.exports = { getItems, getItem, createItem, deleteItem };
